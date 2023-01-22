ab8ed886d7d10dd42709016a502897cd5208fcdf
CheCoelho <checoelho@gmail.com>
2023-01-23 00:27:31 +0200

    feat: prompt builder

---
+++ src/constants/constants.prompts.ts
@@ -0,0 +1,28 @@
+import { getLanguageBundle, SupportedLanguage } from "../types/languages";
+
+export const assemblePrompt = (
+  match: Array<any>,
+  language: SupportedLanguage,
+  code: string
+) =>
+  createPromptIntroduction(match, language) +
+  addDocumentationExpectations() +
+  addCode(code);
+
+export const createPromptIntroduction = (
+  match: Array<any>,
+  language: SupportedLanguage
+) => {
+  const languageBundle = getLanguageBundle(language);
+  return `Create ${languageBundle["language"]} documentation for this function named: "${match[1]}" following the ${languageBundle["documentation_standard"]} standard. `;
+};
+
+export const addDocumentationExpectations = () => {
+  return `Provide a description of the arguments to the function. Ensure to also include a summary of the function and the return of the function.`;
+};
+
+// Add recursive searching for related modules here:
+
+export const addCode = (code: string) => {
+  return `The function code is: ${code}. Ensure not to alter any of the code in your response `;
+};
